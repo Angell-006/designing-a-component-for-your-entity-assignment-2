5529391badfb3969f7fbcd6f033244af2a736f57
Angel <ANGEL.MARY@kalvium.community>
2025-02-21 09:09:44 +0530

    completed the assignment

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,10 @@
 // write button card here
+import React from "react";
+
+const Button =()=>{
+    return (
+        <button className="p-2 bg-blue-400 text-white rounded-md hover:bg-blue-500">View Product</button>
+    );
+};
+
+export default Button;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
 // write product card here
+import React from "react";
+import Button from "./button";
+
+const ProductCard=()=>{
+    const productImg="https://images-cdn.ubuy.co.in/633ff0d45170346bcf2e3c2e-edelweiss-skincare-fridge-mini-makeup.jpg";
+    const productName="Mini Fridge";
+    const productPrice="$450.09";
+
+    return (
+        <div className="product-card">
+            <img src={productImg} alt={productName} className="product-image"></img>
+            <h3 className="product-image">{productName}</h3>
+            <p className="product-price">{productPrice}</p>
+            <Button/>
+        </div>
+    );
+};
+
+export default ProductCard;
